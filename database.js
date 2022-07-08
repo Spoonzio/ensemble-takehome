bd4f3e0f26cbbfe046b54d6d64d8518723940c62
Jason Pan <ypan35@my.bcit.ca>
2022-07-08 16:05:05 -0700

    CRUD core features done

--- database.js
+++ database.js
@@ -23,7 +23,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
             }else{
                 // Table just created, creating some rows
                 var insert = 'INSERT INTO movie (title, description, year, duration, rating) VALUES (?,?,?,?,?)'
-                db.run(insert, ["Titanic", "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", 1997, "194 min", "7.9"])
+                db.run(insert, ["Titanic", "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.", 1997, "194", "7.9"])
             }
         });  
     }
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var express = require("express")
 var app = express()
 var db = require("./database.js")
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Server port
 var HTTP_PORT = 8000 
 // Start server
@@ -45,6 +49,85 @@ app.get("/api/movie/:id", (req, res, next) => {
         })
       });
 });
+
+app.post("/api/movie/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.title){
+        errors.push("No title specified");
+    }
+    if (!req.body.year){
+        errors.push("No year specified");
+    }
+    if (!req.body.duration){
+        errors.push("No duration specified");
+    }
+    if (!req.body.rating){
+        errors.push("No rating specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+
+    var data = {
+        title: req.body.title,
+        description: req.body.description,
+        year: req.body.year,
+        duration: req.body.duration,
+        rating : req.body.rating,
+    }
+
+    var sql = 'INSERT INTO movie (title, description, year, duration, rating) VALUES (?,?,?,?,?)'
+    var params =[data.title, data.description, data.year, data.duration, data.rating]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
+
+app.put("/api/movie/:id", (req, res, next) => {
+    var data = {
+        title: req.body.title,
+        description: req.body.description,
+        year: req.body.year,
+        duration: req.body.duration,
+        rating : req.body.rating,
+    }
+    db.run(
+        'UPDATE movie SET title = ?, description = ?, year = ? , duration = ?, rating = ? WHERE id = ?',
+        [data.title, data.description, data.year, data.duration, data.rating, req.params.id],
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({
+                message: "success",
+                data: data,
+                changes: this.changes
+            })
+    });
+})
+
+app.delete("/api/movie/:id", (req, res, next) => {
+    db.run(
+        'DELETE FROM movie WHERE id = ?',
+        req.params.id,
+        function (err, result) {
+            if (err){
+                res.status(400).json({"error": res.message})
+                return;
+            }
+            res.json({"message":"deleted", changes: this.changes})
+    });
+})
 
 
 // Default response for any other request
